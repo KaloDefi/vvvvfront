1ca3fed36056a3fcdfadba904bceb3baa57c6d00
DeFiDav <86245273+DeFiDav@users.noreply.github.com>
2021-07-03 19:05:14 +0700

    Update EarnAssetCard.tsx

--- src/views/Home/components/EarnAssetCard.tsx
+++ src/views/Home/components/EarnAssetCard.tsx
@@ -25,10 +25,10 @@ const CardMidContent = styled(Heading).attrs({ size: 'xl' })`
   line-height: 44px;
 `
 
-const activeNonCakePools = pools.filter((pool) => !pool.isFinished && !pool.earningToken.symbol.includes('PUSX'))
+const activeNonCakePools = pools.filter((pool) => !pool.isFinished && !pool.earningToken.symbol.includes('VIXA'))
 const latestPools: Pool[] = orderBy(activeNonCakePools, ['sortOrder', 'pid'], ['desc', 'desc']).slice(0, 3)
-// Always include PUSX
-const assets = ['PUSX', ...latestPools.map((pool) => pool.earningToken.symbol)].join(', ')
+// Always include VIXA
+const assets = ['VIXA', ...latestPools.map((pool) => pool.earningToken.symbol)].join(', ')
 
 const EarnAssetCard = () => {
   return (
