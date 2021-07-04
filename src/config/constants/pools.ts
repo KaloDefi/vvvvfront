95064a40d3dfe3ff02ff3fb6339eb443f94e1281
DeFiDav <86245273+DeFiDav@users.noreply.github.com>
2021-07-04 13:44:49 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -16,20 +16,7 @@ const pools: PoolConfig[] = [
     sortOrder: 1,
     isFinished: false,
   },
-   {
-    sousId: 20,
-    stakingToken: tokens.wbnb,
-    earningToken: tokens.vixa,
-    contractAddress: {
-      97: '',
-      56: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '0.2',
-    sortOrder: 2,
-    isFinished: false,
-  },
+  
   
   
  
