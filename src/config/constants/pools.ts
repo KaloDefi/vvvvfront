80200a697e07a7f89a90cc41be1c5746b3b99a2e
DeFiDav <86245273+DeFiDav@users.noreply.github.com>
2021-07-10 11:36:43 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2,20 +2,23 @@ import tokens from './tokens'
 import { PoolConfig, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-  {
-    sousId: 0,
-    stakingToken: tokens.vixa,
-    earningToken: tokens.vixa,
-    contractAddress: {
-      97: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
-      56: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '0.2',
-    sortOrder: 1,
-    isFinished: false,
-  },
+//   {
+//     sousId: 0,
+//     stakingToken: tokens.vixa,
+//     earningToken: tokens.vixa,
+//     contractAddress: {
+//       97: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
+//       56: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
+//     },
+//     poolCategory: PoolCategory.CORE,
+//     harvest: true,
+//     tokenPerBlock: '0.2',
+//     sortOrder: 1,
+//     isFinished: false,
+//   },
+  
+  
+  
   
   
   
