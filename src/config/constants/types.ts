7bb7f68720c64d26bf239514d2b27f01e7dc9a88
DeFiDav <86245273+DeFiDav@users.noreply.github.com>
2021-07-10 10:39:33 +0700

    Update types.ts

--- src/config/constants/types.ts
+++ src/config/constants/types.ts
@@ -1,156 +1,99 @@
-import { TranslatableText } from 'state/types'
-
-export interface Address {
-  97?: string
-  56: string
-}
-
-export interface Token {
-  symbol: string
-  address?: Address
-  decimals?: number
-  projectLink?: string
-}
-
-export enum PoolIds {
-  poolBasic = 'poolBasic',
-  poolUnlimited = 'poolUnlimited',
-}
-
-export type IfoStatus = 'idle' | 'coming_soon' | 'live' | 'finished'
-
-interface IfoPoolInfo {
-  saleAmount: string
-  raiseAmount: string
-  cakeToBurn: string
-  distributionRatio: number // Range [0-1]
-}
+export type IfoStatus = 'coming_soon' | 'live' | 'finished'
 
 export interface Ifo {
   id: string
   isActive: boolean
-  address: string
+  address: Address
   name: string
-  currency: Token
-  token: Token
-  releaseBlockNumber: number
-  articleUrl: string
-  campaignId: string
-  tokenOfferingPrice: number
-  isV1: boolean
-  [PoolIds.poolBasic]?: IfoPoolInfo
-  [PoolIds.poolUnlimited]: IfoPoolInfo
+  token: string
+  maxContribution: string
+  minContribution: string
+  subTitle?: string
+  description?: string
+  saleAmount: string
+  raiseAmount: string
+  projectSiteUrl: string
+  currency: string
+  currencyAddress: string
+  tokenDecimals: number
+}
+
+export enum QuoteToken {
+  'BNB' = 'BNB',
+  'WBNB' = 'WBNB',
+  'SALT' = 'SALT',
+  'SUSHI' = 'SUSHI',
+  'CAKE' = 'CAKE',
+  'SYRUP' = 'SYRUP',
+  'BUSD' = 'BUSD',
+  'ETH' = 'ETH',
+  'TWT' = 'TWT',
+  'UST' = 'UST',
+  'SALTBLUE' = 'SALT-BLUE',
+  'SALTSLME' = 'SALT-SLME',
+  'SALTBUSD' = 'SALT-BUSD',
+  'SALTMOMO' = 'SALT-MOMO',
+  'SALTPALM' = 'SALT-PALM',
+  'SALTMCH' = 'SALT-MCH',
 }
 
 export enum PoolCategory {
   'COMMUNITY' = 'Community',
   'CORE' = 'Core',
   'BINANCE' = 'Binance', // Pools using native BNB behave differently than pools using a token
-  'AUTO' = 'Auto',
+}
+
+export interface Address {
+  97?: string
+  56: string
 }
 
 export interface FarmConfig {
   pid: number
   lpSymbol: string
   lpAddresses: Address
-  token: Token
-  quoteToken: Token
+  tokenSymbol: string
+  tokenAddresses: Address
+  quoteTokenSymbol: QuoteToken
+  quoteTokenAdresses: Address
+  rewardTokenAddresses?: Address
+  isMasterChef: boolean
   multiplier?: string
+  isTokenOnly?: boolean
   isCommunity?: boolean
+  risk: number
   dual?: {
     rewardPerBlock: number
     earnLabel: string
     endBlock: number
   }
-  isKingdom?: boolean
-  isTokenOnly?: boolean
 }
 
 export interface PoolConfig {
   sousId: number
-  earningToken: Token
-  stakingToken: Token
+  image?: string
+  tokenName: string
+  stakingTokenName: QuoteToken
   stakingLimit?: number
+  stakingTokenAddress?: Address
   contractAddress: Address
+  rewardTokenAddress: Address
   poolCategory: PoolCategory
+  projectLink: string
   tokenPerBlock: string
   sortOrder?: number
   harvest?: boolean
   isFinished?: boolean
-}
-
-export type Images = {
-  lg: string
-  md: string
-  sm: string
-  ipfs?: string
-}
-
-export type NftImages = {
-  blur?: string
-} & Images
-
-export type NftVideo = {
-  webm: string
-  mp4: string
-}
-
-export type NftSource = {
-  [key in NftType]: {
-    address: Address
-    identifierKey: string
-  }
-}
-
-export enum NftType {
-  PANCAKE = 'pancake',
-  MIXIE = 'mixie',
+  tokenDecimals: number
+  burnFee: number
 }
 
 export type Nft = {
-  description: string
   name: string
-  images: NftImages
+  description: string
+  originalImage: string
+  previewImage: string
+  blurImage: string
   sortOrder: number
-  type: NftType
-  video?: NftVideo
-
-  // Uniquely identifies the nft.
-  // Used for matching an NFT from the config with the data from the NFT's tokenURI
-  identifier: string
-
-  // Used to be "bunnyId". Used when minting NFT
-  variationId?: number | string
-}
-
-export type TeamImages = {
-  alt: string
-} & Images
-
-export type Team = {
-  id: number
-  name: string
-  description: string
-  isJoinable?: boolean
-  users: number
-  points: number
-  images: TeamImages
-  background: string
-  textColor: string
-}
-
-export type CampaignType = 'ifo' | 'teambattle'
-
-export type Campaign = {
-  id: string
-  type: CampaignType
-  title?: TranslatableText
-  description?: TranslatableText
-  badge?: string
-}
-
-export type PageMeta = {
-  title: string
-  description?: string
-  image?: string
+  bunnyId: number
 }
