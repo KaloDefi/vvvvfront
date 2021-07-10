06f2eef1fcf4ceb206b5d345c82dffc4ab6979b1
DeFiDav <86245273+DeFiDav@users.noreply.github.com>
2021-07-10 11:00:03 +0700

    Update types.ts

--- src/config/constants/types.ts
+++ src/config/constants/types.ts
@@ -1,5 +1,16 @@
 export type IfoStatus = 'coming_soon' | 'live' | 'finished'
 
+export interface Address {
+  97?: string
+  56: string
+}
+
+export interface Token {
+  symbol: string
+  address?: Address
+  decimals?: number
+  projectLink?: string
+}
 export enum PoolIds {
   poolBasic = 'poolBasic',
   poolUnlimited = 'poolUnlimited',
