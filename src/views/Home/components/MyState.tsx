74446354eeb95f1d1b6d89bf89b5b2a1c4e4180a
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-21 14:10:34 +0700

    Update MyState.tsx

--- src/views/Home/components/MyState.tsx
+++ src/views/Home/components/MyState.tsx
@@ -73,7 +73,7 @@ const MyState = () => {
           <Label>{t('Date: Sun Aug 01 2021 09:30:00 GMT+0700')}</Label>
         </Block>
         <Actions>
-            <Button onclick="location.href='https://bscscan.com/block/countdown/9655555'"
+            <Button onClick="location.href='https://bscscan.com/block/countdown/9655555'"
               width="100%">
               COUNTDOWN LINK
           </Button>
