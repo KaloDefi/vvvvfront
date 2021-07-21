539698ce1d55b320a2ed688441381b68de9b5c5a
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-21 09:51:08 +0700

    Update HarvestAction.tsx

--- src/views/Farms/components/FarmTable/Actions/HarvestAction.tsx
+++ src/views/Farms/components/FarmTable/Actions/HarvestAction.tsx
@@ -48,7 +48,7 @@ const HarvestAction: React.FunctionComponent<HarvestActionProps> = ({ pid, userD
   return (
     <ActionContainer>
       <ActionTitles>
-        <Title>ViXA</Title>
+        <Title>KOLO </Title>
         <Subtle>{t('EARNED')}</Subtle>
       </ActionTitles>
       <ActionContent>
