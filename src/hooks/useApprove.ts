4c0481dd323cda240b8b3c88c72414ce2556656a
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-21 16:26:15 +0700

    Update useApprove.ts

--- src/hooks/useApprove.ts
+++ src/hooks/useApprove.ts
@@ -1,64 +1,135 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { Contract } from 'web3-eth-contract'
 import { ethers } from 'ethers'
+import BigNumber from 'bignumber.js'
 import { useAppDispatch } from 'state'
-import { updateUserAllowance, fetchFarmUserDataAsync } from 'state/actions'
+import { updateUserAllowance } from 'state/actions'
 import { approve } from 'utils/callHelpers'
-import { useMasterchef, useCake, useSousChef, useLottery } from './useContract'
+import { useTranslation } from 'contexts/Localization'
+import { useMasterchef, useCrystl, useSousChef, useLottery, useCrystlVaultContract } from './useContract'
+import useToast from './useToast'
+import useLastUpdated from './useLastUpdated'
 
 // Approve a Farm
 export const useApprove = (lpContract: Contract) => {
-  const dispatch = useAppDispatch()
   const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
 
   const handleApprove = useCallback(async () => {
     try {
       const tx = await approve(lpContract, masterChefContract, account)
-      dispatch(fetchFarmUserDataAsync(account))
       return tx
     } catch (e) {
       return false
     }
-  }, [account, dispatch, lpContract, masterChefContract])
+  }, [account, lpContract, masterChefContract])
 
   return { onApprove: handleApprove }
 }
 
 // Approve a Pool
-export const useSousApprove = (lpContract: Contract, sousId) => {
+export const useSousApprove = (lpContract: Contract, sousId, earningTokenSymbol) => {
+  const [requestedApproval, setRequestedApproval] = useState(false)
+  const { toastSuccess, toastError } = useToast()
+  const { t } = useTranslation()
   const dispatch = useAppDispatch()
   const { account } = useWeb3React()
   const sousChefContract = useSousChef(sousId)
 
   const handleApprove = useCallback(async () => {
     try {
+      setRequestedApproval(true)
       const tx = await approve(lpContract, sousChefContract, account)
       dispatch(updateUserAllowance(sousId, account))
-      return tx
+      if (tx) {
+        toastSuccess(
+          t('Contract Enabled'),
+          t('You can now stake in the %symbol% pool!', { symbol: earningTokenSymbol }),
+        )
+        setRequestedApproval(false)
+      } else {
+        // user rejected tx or didn't go thru
+        toastError(t('Error'), t('Please try again. Confirm the transaction and make sure you are paying enough gas!'))
+        setRequestedApproval(false)
+      }
     } catch (e) {
-      return false
+      console.error(e)
+      toastError(t('Error'), e?.message)
     }
-  }, [account, dispatch, lpContract, sousChefContract, sousId])
+  }, [account, dispatch, lpContract, sousChefContract, sousId, earningTokenSymbol, t, toastError, toastSuccess])
 
-  return { onApprove: handleApprove }
+  return { handleApprove, requestedApproval }
+}
+
+// Approve CRYSTL auto pool
+export const useVaultApprove = (setLastUpdated: () => void) => {
+  const { account } = useWeb3React()
+  const [requestedApproval, setRequestedApproval] = useState(false)
+  const { t } = useTranslation()
+  const { toastSuccess, toastError } = useToast()
+  const crystlVaultContract = useCrystlVaultContract()
+  const crystlContract = useCrystl()
+
+  const handleApprove = () => {
+    crystlContract.methods
+      .approve(crystlVaultContract.options.address, ethers.constants.MaxUint256)
+      .send({ from: account })
+      .on('sending', () => {
+        setRequestedApproval(true)
+      })
+      .on('receipt', () => {
+        toastSuccess(t('Contract Enabled'), t('You can now stake in the %symbol% vault!', { symbol: 'CRYSTL' }))
+        setLastUpdated()
+        setRequestedApproval(false)
+      })
+      .on('error', (error) => {
+        console.error(error)
+        toastError(t('Error'), t('Please try again. Confirm the transaction and make sure you are paying enough gas!'))
+        setRequestedApproval(false)
+      })
+  }
+
+  return { handleApprove, requestedApproval }
+}
+
+export const useCheckVaultApprovalStatus = () => {
+  const [isVaultApproved, setIsVaultApproved] = useState(false)
+  const { account } = useWeb3React()
+  const crystlContract = useCrystl()
+  const crystlVaultContract = useCrystlVaultContract()
+  const { lastUpdated, setLastUpdated } = useLastUpdated()
+  useEffect(() => {
+    const checkApprovalStatus = async () => {
+      try {
+        const response = await crystlContract.methods.allowance(account, crystlVaultContract.options.address).call()
+        const currentAllowance = new BigNumber(response)
+        setIsVaultApproved(currentAllowance.gt(0))
+      } catch (error) {
+        setIsVaultApproved(false)
+      }
+    }
+
+    checkApprovalStatus()
+  }, [account, crystlContract, crystlVaultContract, lastUpdated])
+
+  return { isVaultApproved, setLastUpdated }
 }
 
 // Approve the lottery
 export const useLotteryApprove = () => {
   const { account } = useWeb3React()
-  const cakeContract = useCake()
+  const crystlContract = useCrystl()
   const lotteryContract = useLottery()
 
   const handleApprove = useCallback(async () => {
     try {
-      const tx = await approve(cakeContract, lotteryContract, account)
+      const tx = await approve(crystlContract, lotteryContract, account)
       return tx
     } catch (e) {
       return false
     }
-  }, [account, cakeContract, lotteryContract])
+  }, [account, crystlContract, lotteryContract])
 
   return { onApprove: handleApprove }
 }
