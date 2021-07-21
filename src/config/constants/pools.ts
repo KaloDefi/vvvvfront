a2e8cb04ee51bafb4b6bdd29e7e40289a12ecffc
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-21 09:48:04 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -4,8 +4,8 @@ import { PoolConfig, PoolCategory } from './types'
 const pools: PoolConfig[] = [
   {
     sousId: 0,
-    stakingToken: tokens.vixa,
-    earningToken: tokens.vixa,
+    stakingToken: tokens.kolo,
+    earningToken: tokens.kolo,
     contractAddress: {
       97: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
       56: '0xed7459319b9a6ccf3b8e6f96f1e5b70ea83a0b6d',
