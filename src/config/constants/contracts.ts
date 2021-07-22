fb63de0ecaaffd67f380d5428e861f01ef161369
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-22 14:40:04 +0700

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,66 +1,66 @@
 export default {
   masterChef: {
     97: '0x329d643e4a2e15f98d538a5d186a50dbd3da60ba',
-    56: '0x329d643e4a2e15f98d538a5d186a50dbd3da60ba',
+    137: '0x329d643e4a2e15f98d538a5d186a50dbd3da60ba',
   },
   sousChef: {
     97: '',
-    56: '0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95',
+    137: '0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95',
   },
   lottery: {
     97: '',
-    56: '0x091fF4ee8b12dF117a9b3A9F3D7E8fd5f6F79ac1',
+    137: '0x091fF4ee8b12dF117a9b3A9F3D7E8fd5f6F79ac1',
   },
   lotteryNFT: {
     97: '0x8175c10383511b3a1C68f9dB222dc14A19CC950e',
-    56: '',
+    137: '',
   },
   multiCall: {
     56: '0x162e570360dd5B13d37FDffA198F8F310bb3FF06',
-    97: '0x162e570360dd5B13d37FDffA198F8F310bb3FF06',
+    137: '0x162e570360dd5B13d37FDffA198F8F310bb3FF06',
   },
   pancakeProfile: {
-    56: '0xDf4dBf6536201370F95e06A0F8a7a70fE40E388a',
+    137: '0xDf4dBf6536201370F95e06A0F8a7a70fE40E388a',
     97: '',
   },
   pancakeRabbits: {
-    56: '0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07',
+    137: '0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07',
     97: '',
   },
   bunnyFactory: {
-    56: '0xfa249Caa1D16f75fa159F7DFBAc0cC5EaB48CeFf',
+    137: '0xfa249Caa1D16f75fa159F7DFBAc0cC5EaB48CeFf',
     97: '',
   },
   claimRefund: {
-    56: '0xE7e53A7e9E3Cf6b840f167eF69519175c497e149',
+    137: '0xE7e53A7e9E3Cf6b840f167eF69519175c497e149',
     97: '',
   },
   pointCenterIfo: {
-    56: '0x3C6919b132462C1FEc572c6300E83191f4F0012a',
+    137: '0x3C6919b132462C1FEc572c6300E83191f4F0012a',
     97: '',
   },
   bunnySpecial: {
-    56: '0xFee8A195570a18461146F401d6033f5ab3380849',
+    137: '0xFee8A195570a18461146F401d6033f5ab3380849',
     97: '',
   },
   tradingCompetition: {
-    56: '0xd718baa0B1F4f70dcC8458154042120FFE0DEFFA',
+    137: '0xd718baa0B1F4f70dcC8458154042120FFE0DEFFA',
     97: '',
   },
   easterNft: {
-    56: '0x23c41D28A239dDCAABd1bb1deF8d057189510066',
+    137: '0x23c41D28A239dDCAABd1bb1deF8d057189510066',
     97: '',
   },
   cakeVault: {
-    56: '',
+    137: '',
     97: '',
   },
   predictions: {
-    56: '0x516ffd7D1e0Ca40b1879935B2De87cb20Fc1124b',
+    137: '0x516ffd7D1e0Ca40b1879935B2De87cb20Fc1124b',
     97: '',
   },
   referrals: {
-    56: '0x81bf3b9883ee8e72457a54404b9c03be3970cc01',
+    137: '0x81bf3b9883ee8e72457a54404b9c03be3970cc01',
     97: '0x81bf3b9883ee8e72457a54404b9c03be3970cc01',
   },
 }
