45cdd3dc41bad7b62ada257e756ec0d6fd6b413b
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-22 14:58:43 +0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,13 +11,13 @@ export const BSC_BLOCK_TIME = 3
 // BECO_PER_BLOCK details
 // 70 BECO is minted per block
 
-export const PUSX_PER_BLOCK = new BigNumber(1)
+export const PUSX_PER_BLOCK = new BigNumber(0.5)
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
-export const BASE_URL = 'https://puswap.com/'
-export const BASE_EXCHANGE_URL = 'https://exchange.pancakeswap.finance'
+export const BASE_URL = 'https://kolo.finance/'
+export const BASE_EXCHANGE_URL = 'https://quickswap.exchange/'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
 export const BASE_LIQUIDITY_POOL_URL = `${BASE_EXCHANGE_URL}/#/pool`
-export const BASE_BSC_SCAN_URL = 'https://bscscan.com'
+export const BASE_BSC_SCAN_URL = 'https://polygonscan.com'
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50
 export const LOTTERY_TICKET_PRICE = 100
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
