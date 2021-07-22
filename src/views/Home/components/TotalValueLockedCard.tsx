9d924d175282b2b3f627768d7f55fdc1997c2aa8
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-22 13:46:47 +0700

    Update TotalValueLockedCard.tsx

--- src/views/Home/components/TotalValueLockedCard.tsx
+++ src/views/Home/components/TotalValueLockedCard.tsx
@@ -1,36 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Card, CardBody, Heading, Skeleton, Text } from '@becoswap-libs/uikit'
+import { Card, CardBody, Heading, Skeleton, Text, Flex, ArrowForwardIcon } from '@crystals/uikit'
+import { NavLink } from 'react-router-dom'
 import { useTranslation } from 'contexts/Localization'
-import { useGetStats } from 'hooks/api'
+import { useTotalValue } from 'state/hooks'
+import { getCrystlAddress } from 'utils/addressHelpers'
+import getTokenInfoUrl from 'utils/getTokenInfoUrl'
+import CardValue from './CardValue'
 
 const StyledTotalValueLockedCard = styled(Card)`
-  align-items: center;
-  display: flex;
-  flex: 1;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    margin: 0;
+    max-width: none;
+  }
+
+  transition: opacity 200ms;
+  &:hover {
+    opacity: 0.65;
+  }
 `
 
 const TotalValueLockedCard = () => {
   const { t } = useTranslation()
-  const data = useGetStats()
-  const tvl = data ? data.tvl.toLocaleString('en-US', { maximumFractionDigits: 0 }) : null
+  const data = useTotalValue()
+  const tvl = data
 
   return (
     <StyledTotalValueLockedCard>
-      <CardBody>
-        <Heading size="lg" mb="24px">
-          {t('Total Value Locked (TVL)')}
-        </Heading>
-        {data ? (
-          <>
-
-            <Heading size="xl">{t('$20,051.29')}</Heading>
-            <Text color="textSubtle">{t('Across all LPs and ViXA Pools')}</Text>
-          </>
-        ) : (
-          <Skeleton height={66} />
-        )}
-      </CardBody>
+      <NavLink
+        exact
+        activeClassName="active"
+        to={{ pathname: getTokenInfoUrl(getCrystlAddress()) }}
+        target="_blank"
+        id="farm-apr-cta"
+      >
+        <CardBody>
+          <Heading color="contrast" scale="lg" mb="24px">
+            {t('Total Value Locked (TVL)')}
+          </Heading>
+          {data ? (
+            <>
+              <CardValue value={tvl.toNumber()} prefix="$" decimals={2} />
+              <Flex justifyContent="space-between">
+                <Text color="textSubtle">{t('Across all LPs and KOLO Pools')}</Text>
+                <ArrowForwardIcon mt={30} color="primary" />
+              </Flex>
+            </>
+          ) : (
+            <Skeleton animation="pulse" variant="rect" height="44px" />
+          )}
+        </CardBody>
+      </NavLink>
     </StyledTotalValueLockedCard>
   )
 }
