aaf432022cb31b2d32fa8496c96797b86ed00556
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-07-22 14:15:02 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1,57 +1,39 @@
 const tokens = {
-  bnb: {
-    symbol: 'BNB',
-    projectLink: 'https://www.binance.com/',
+  matic: {
+    symbol: 'MATIC',
+    projectLink: 'https://www.matic.network/',
   },
   kolo: {
     symbol: 'KOLO',
     address: {
-      56: '0x236f2345030668ccb317b3ee8f7916b962be2321',
+      137: '0x236f2345030668ccb317b3ee8f7916b962be2321',
       97: '',
     },
     decimals: 18,
     projectLink: 'https://kolo.finance',
   },
-  btcb: {
-    symbol: 'BTCB',
+  wmatic: {
+    symbol: 'WMATIC',
     address: {
-      56: '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c',
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+      97: '',
     },
     decimals: 18,
-    projectLink: 'https://bitcoin.org/',
-  },
-  wbnb: {
-    symbol: 'wBNB',
-    address: {
-      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
-      97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
-    },
-    decimals: 18,
-    projectLink: 'https://pancakeswap.finance/',
+    projectLink: 'https://www.matic.network/',
   },
   link: {
     symbol: 'LINK',
     address: {
-      56: '0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd',
+      56: '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39',
       97: '',
     },
     decimals: 18,
     projectLink: 'https://chain.link/',
   },
-  busd: {
-    symbol: 'BUSD',
+  weth: {
+    symbol: 'WETH',
     address: {
-      56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://www.paxos.com/busd/',
-  },
-  eth: {
-    symbol: 'ETH',
-    address: {
-      56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+      137: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
       97: '',
     },
     decimals: 18,
@@ -60,139 +42,29 @@ const tokens = {
   usdc: {
     symbol: 'USDC',
     address: {
-      56: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
+      137: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
       97: '',
     },
     decimals: 18,
     projectLink: 'https://www.centre.io/usdc',
   },
-  cake: {
-    symbol: 'CAKE',
-    address: {
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
-    },
-    decimals: 18,
-    projectLink: 'https://pancakeswap.finance/',
-  },
   usdt: {
     symbol: 'USDT',
     address: {
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-      56: '0x55d398326f99059ff775485246999027b3197955',
+      97: '',
+      137: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
     },
     decimals: 18,
     projectLink: 'https://tether.to/',
   },
-
-  tusd: {
-    symbol: 'TUSD',
-    address: {
-      56: '0x14016e85a25aeb13065688cafb43044c2ef86784',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://www.trueusd.com/',
-  },
-  ust: {
-    symbol: 'UST',
-    address: {
-      56: '0x23396cf899ca06c4472205fc903bdb4de249d6fc',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://mirror.finance/',
-  },
   dai: {
     symbol: 'DAI',
     address: {
-      56: '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3',
+      137: '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063',
       97: '',
     },
     decimals: 18,
     projectLink: 'http://www.makerdao.com/',
-  },
-
-  ltc: {
-    symbol: 'LTC',
-    address: {
-      56: '0x4338665cbb7b2485a8855a139b75d5e34ab0db94',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://litecoin.org/',
-  },
-  xrp: {
-    symbol: 'XRP',
-    address: {
-      56: '0x1d2f0da169ceb9fc7b3144628db156f3f6c60dbe',
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-    },
-    decimals: 18,
-    projectLink: 'https://ripple.com/xrp/',
-  },
-  uni: {
-    symbol: 'UNI',
-    address: {
-      56: '0xbf5140a22578168fd562dccf235e5d43a02ce9b1',
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-    },
-    decimals: 18,
-    projectLink: 'https://uniswap.org/',
-  },
-  vai: {
-    symbol: 'VAI',
-    address: {
-      56: '0x4bd17003473389a42daf6a0a729f6fdb328bbbd7',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://venus.io/',
-  },
-  doge: {
-    symbol: 'DOGE',
-    address: {
-      56: '0xba2ae424d960c26247dd6c32edc70b295c744c43',
-      97: '',
-    },
-    decimals: 8,
-    projectLink: 'https://dogecoin.com/',
-  },
-  trx: {
-    symbol: 'TRX',
-    address: {
-      56: '0x85eac5ac2f758618dfa09bdbe0cf174e7d574d5b',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://tron.network/',
-  },
-  shib: {
-    symbol: 'SHIB',
-    address: {
-      56: '0x2859e4544c4bb03966803b044a93563bd2d0dd4d',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://shibatoken.com/',
-  },
-  dot: {
-    symbol: 'DOT',
-    address: {
-      56: '0x7083609fce4d1d8dc0c979aab8c869ea2c873402',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://polkadot.network/',
-  },
-  ada: {
-    symbol: 'ADA',
-    address: {
-      56: '0x3ee2200efb3400fabb9aacf31297cbdd1d435d47',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://cardano.org/',
   },
 }
 
